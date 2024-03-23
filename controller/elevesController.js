d9a645ce63924f5167c4ba432814d0565adc0fa6
koffiemmal <137209739+koffiemmal@users.noreply.github.com>
2024-03-23 00:04:04 +0000

    gestion des eleves

--- controller/elevesController.js
+++ controller/elevesController.js
@@ -21,10 +21,101 @@ exports.listesEleves=(req,res)=>{
 
     database.query(listesshow,(error,result)=>{
         if(error){
-            res.satus(400)
+            res.status(400)
         }
         else{
             res.status(200).json(result)
     }})
 
+}
+
+exports.getElevesbyName =(req,res)=>{
+
+    let infoseleves = "SELECT * FROM eleves WHERE nom_eleves=?;"
+
+    database.query(infoseleves,[req.body.nom_eleves],(error,results)=> {
+        if(error){
+            res.status(400)
+        }
+        else{
+            res.status(200).json(results)
+    }
+
+    })
+
+}
+exports.getElevesbySurname =(req,res)=>{
+
+    let infoseleves = "SELECT * FROM eleves WHERE prenom_eleves=?;"
+
+    database.query(infoseleves,[req.body.prenom_eleves],(error,results)=> {
+        if(error){
+            res.status(400)
+        }
+        else{
+            res.status(200).json(results)
+    }
+
+    })
+
+}
+exports.getElevesbyId =(req,res)=>{
+
+    let infoseleves = "SELECT * FROM eleves WHERE id_eleves=?;"
+
+    database.query(infoseleves,[req.body.id_eleves],(error,results)=> {
+        if(error){
+            res.status(400)
+        }
+        else{
+            res.status(200).json(results)
+    }
+
+    })
+
+}
+
+exports.modifierNom = (req,res)=>{
+
+    /* const { nom_eleves, id_eleves } = req.body; */
+
+    let modificationNom = "UPDATE eleves SET nom_eleves = ? WHERE id_eleves = ?;"
+
+    database.query(modificationNom,[req.body.nom_eleves,req.body.id_eleves],(error,result)=>{
+        if(error){
+            return res.status(500).json({ error: 'Erreur lors de la modification du nom de l\'élève.' });
+        }
+        else{
+            res.status(200).json(result)
+    }
+
+    })
+}
+exports.modifierPrenom = (req,res)=>{
+
+    let modificationPrenom = "UPDATE eleves SET prenom_eleves = ? WHERE id_eleves = ?;"
+
+    database.query(modificationPrenom,[req.body.prenom_eleves,req.body.id_eleves],(error,result)=>{
+        if(error){
+            return res.status(500).json({ error: 'Erreur lors de la modification du prenom de l\'élève.' });
+        }
+        else{
+            res.status(200).json(result)
+    }
+
+    })
+}
+exports.modifierClasse = (req,res)=>{
+
+    let modificationClasse = "UPDATE eleves SET classe_eleves = ? WHERE id_eleves = ?;"
+
+    database.query(modificationClasse,[req.body.classe_eleves,req.body.id_eleves],(error,result)=>{
+        if(error){
+            return res.status(500).json({ error: 'Erreur lors de la modification de la classe de l\'élève.' });
+        }
+        else{
+            res.status(200).json(result)
+    }
+
+    })
 }
--- routes/elevesRoute.js
+++ routes/elevesRoute.js
@@ -5,7 +5,19 @@ const elevesController = require("../controller/elevesController")
 
 router.post("/ajoutEleves",elevesController.ajoutEleves)
 
+router.put("/modifierNom",elevesController.modifierNom)
+
+router.put("/modifierPrenom",elevesController.modifierPrenom)
+
+router.put("/modifierClasse",elevesController.modifierClasse)
+
 router.get("/listesEleves",elevesController.listesEleves)
+
+router.get("/getElevesbyId",elevesController.getElevesbyId)
+
+router.get("/getElevesbyName",elevesController.getElevesbyName)
+
+router.get("/getElevesbySurname",elevesController.getElevesbySurname)
 
 
 module.exports = router
